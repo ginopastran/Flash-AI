7f788bf1c8afdfb15a15954c8e1b0e442db59cd7
ginopastran <ginopastran@gmail.com>
2023-09-08 20:57:26 -0300

    final features 3

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -56,7 +56,7 @@ const DashboardPage = () => {
 
   return (
     <div>
-      <div className=" mb-8 space-y-4">
+      <div className=" mb-8 space-y-4 ">
         <h2 className=" text-2xl md:text-4xl font-bold text-center">
           Explore the power of AI
         </h2>
@@ -64,7 +64,7 @@ const DashboardPage = () => {
           Chat with the smartest AI - Experience the power of AI
         </p>
       </div>
-      <div className=" px-4 md:px-20 lg:px-32 space-y-4">
+      <div className=" px-4 md:px-20 lg:px-32 space-y-4 pb-6">
         {tools.map((tool) => (
           <Card
             onClick={() => router.push(tool.href)}
